62565a387b48343b43c6049172c2f698803994a1
cieloenfer <105323721+cieloenfer@users.noreply.github.com>
2024-04-08 20:37:39 -0400

    DesafíoClase15

---
+++ src/Dao/Models/cartModel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const cartSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true }],
+    createdAt: { type: Date, default: Date.now }
+});
+
+const Cart = mongoose.model('Cart', cartSchema);
+
+module.exports = Cart;
+
